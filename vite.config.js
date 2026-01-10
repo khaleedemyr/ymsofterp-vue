683f0bcb3617609fb5a3138c0e5562b348b46962
khaleedemyr <hendiroom@gmail.com>
2026-01-10 14:13:11 +0700

    buku bank

--- vite.config.js
+++ vite.config.js
@@ -25,5 +25,16 @@ export default defineConfig({
         commonjsOptions: {
             include: [/firebase/, /node_modules/],
         },
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'vendor-vue': ['vue', 'vue-router', '@inertiajs/vue3'],
+                    'vendor-charts': ['apexcharts', 'vue3-apexcharts'],
+                    'vendor-ui': ['element-plus', '@headlessui/vue'],
+                    'vendor-utils': ['axios', 'dayjs', 'date-fns'],
+                },
+            },
+        },
     },
 });
