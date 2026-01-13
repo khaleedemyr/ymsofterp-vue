52c99d97c64f20fe0bfe9ad1ce57872bd09aa3cd
khaleedemyr <hendiroom@gmail.com>
2026-01-13 10:12:39 +0700

    buku bank

--- resources/js/Pages/FoodPayment/Form.vue
+++ resources/js/Pages/FoodPayment/Form.vue
@@ -364,13 +364,14 @@
       </div>
 
       <!-- Preview Modal -->
-      <div v-if="showPreviewModal" class="fixed inset-0 z-50 overflow-y-auto" @click.self="showPreviewModal = false">
-        <div class="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-          <!-- Background overlay -->
-          <div class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" @click="showPreviewModal = false"></div>
-
+      <div v-if="showPreviewModal" class="fixed inset-0 z-50 overflow-y-auto">
+        <!-- Background overlay -->
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="showPreviewModal = false" style="z-index: 1;"></div>
+        
+        <!-- Modal container -->
+        <div class="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0 relative" style="z-index: 2;">
           <!-- Modal panel -->
-          <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-5xl sm:w-full">
+          <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-5xl sm:w-full relative" @click.stop>
             <!-- Header -->
             <div class="bg-gradient-to-r from-blue-600 to-indigo-600 px-6 py-4 flex items-center justify-between">
               <h3 class="text-lg font-semibold text-white">
